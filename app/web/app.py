a394542270ee0d324315a8a886a434041860dbbb
Safonov <Sofik.d@gmail.com>
2024-04-15 17:56:25 +0300

    Добавление ключа кук и аписпека

--- app/web/app.py
+++ app/web/app.py
@@ -1,8 +1,15 @@
+import base64
+
+
 from aiohttp.web import (
     Application as AiohttpApplication,
     Request as AiohttpRequest,
     View as AiohttpView,
 )
+from aiohttp_apispec import setup_aiohttp_apispec
+from aiohttp_session import setup as session_setup
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+
 
 from app.admin.models import Admin
 from app.store import Store, setup_store
@@ -48,6 +55,11 @@ def setup_app(config_path: str) -> Application:
     setup_logging(app)
     setup_config(app, config_path)
     setup_routes(app)
+    secret_key = base64.urlsafe_b64encode("Thirty  two  length  byt".encode('utf-8'))
+    # aiohttp_session.setup(app, storage=EncryptedCookieStorage(secret_key))
+    session_setup(app, storage=EncryptedCookieStorage(
+        secret_key=secret_key))
     setup_middlewares(app)
     setup_store(app)
+    setup_aiohttp_apispec(app, title="Our application", url="/docs/json", swagger_path="/docs")
     return app
